3dcd37e152ff2aa1737c4d1550836b3dd686d53e
Idlidev <idliyout@gmail.com>
2021-11-28 19:03:47 +0530

    *colours*

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -8,13 +8,16 @@ void kernel_main()
 {
     print_clear();
 
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     print_str("Installing interrupt service routines (ISRs).\n");
 
     isr_install();
     
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     print_str("Enabling external interrupts.\n");
     asm volatile("sti");
 
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     print_str("Initializing keyboard (IRQ 1).\n");
 
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
@@ -29,9 +32,11 @@ void execute_command(char *input)
 {
     if (compare_string(input, "EXIT") == 0)
     {
+		print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
         print_str("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
     }
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
     print_str("Unknown command: ");
     print_str(input);
     print_str("\n> ");
